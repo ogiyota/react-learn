85b23da6c464ef45ba8461e45e225de9ccabe91e
yota ogihara <sponge3039@gmail.com>
2022-10-14 00:47:12 +0900

    button3

--- src/App.js
+++ src/App.js
@@ -304,8 +304,8 @@ const handleClick = (e)=>{
         <h3>{qs.Q}</h3>
       </div>
       <div className='buttona'>
-        <button value={"○"} onClick={ handleClick} >{answer[0]}</button>
-        <button value={"×"} onClick={ handleClick} >{answer[1]}</button>
+        <button value={"○"} className="buttonb" onClick={ handleClick} >{answer[0]}</button>
+        <button value={"×"} className="buttonb" onClick={ handleClick} >{answer[1]}</button>
         <p className='Q'>出題数：{count}</p>
         <p className='C'>正解数：{correct}</p>
         <div className='quiz'>
